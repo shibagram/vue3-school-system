c5800b751a4cd53ef4c0314e206360abc49be28c
shibagram <masaki.ttt567@gmail.com>
2022-11-11 22:33:13 +0900

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC_EALMoO7b-Mj4feWnF8Ic75F4Ms16zOc",
+  authDomain: "vue3-school-system-cd7ed.firebaseapp.com",
+  projectId: "vue3-school-system-cd7ed",
+  storageBucket: "vue3-school-system-cd7ed.appspot.com",
+  messagingSenderId: "230968419887",
+  appId: "1:230968419887:web:e719b0de7e15c5ef08c8da",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

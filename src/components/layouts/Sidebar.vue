1c5e2603b00fd404ca7ad77a2f704c55edfb6f99
shibagram <masaki.ttt567@gmail.com>
2022-12-03 12:45:30 +0900

    ログアウト機能を実装

--- src/components/layouts/Sidebar.vue
+++ src/components/layouts/Sidebar.vue
@@ -6,12 +6,27 @@
 
 <script setup lang="ts">
 import { ref } from "vue";
+import { auth } from "@/firebase";
+import { signOut } from "firebase/auth";
+import { useRouter } from 'vue-router'
 
+const router = useRouter();
 const links = ref([
-  ['mdi-view-dashboard', 'Inbox'],
+  ['mdi-home', 'Inbox'],
   ['mdi-account-box', 'Send'],
   ['mdi-gavel', 'Trash'],
 ])
+
+const logout = async () => {
+  try {
+    await signOut(auth);
+    localStorage.successMessage = "ログアウトに成功しました。"
+    router.push("/login")
+    console.log("logout")
+  } catch (error) {
+    console.log(error);
+  }
+}
 </script>
 
 <template>
@@ -33,7 +48,7 @@ const links = ref([
 
     <template v-slot:append>
       <div class="pa-2">
-        <v-btn block>
+        <v-btn @click="logout" block>
           Logout
         </v-btn>
       </div>
--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -8,11 +8,16 @@
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
 
-// Composables
 import { createVuetify } from 'vuetify'
+import { mdi } from 'vuetify/iconsets/mdi-svg'
 
-// https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+    sets: {
+      mdi,
+    },
+  },
   theme: {
     themes: {
       light: {

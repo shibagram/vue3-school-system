a259a1346047a6a39e0b7781e6191c658ae91751
shibagram <masaki.ttt567@gmail.com>
2022-12-16 23:51:58 +0900

    Cloud Functionsからfirestoreのデータを取得する処理（下書き）

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,16 @@
 import * as functions from "firebase-functions";
 
-
 // The Firebase Admin SDK to access Firestore.
 // const admin = require('firebase-admin');
 // admin.initializeApp();
 
 export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", {structuredData: true});
+  functions.logger.info("Hello logs!", { structuredData: true });
   response.send("Hello from Firebase!");
 });
+
+// export const getBookInfo = functions.https.onRequest(async (request, response) => {
+//   const doc = await db.collection('books').doc('1').get()
+//   const bookInfo = doc.data()
+//   response.send(bookInfo);
+// });
